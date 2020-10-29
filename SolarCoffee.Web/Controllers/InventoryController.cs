83437500594a0b5877373665a00932822cb90e91
Daniel Neiva <danielneiva5@outlook.com>
2020-10-29 18:05:31 +0000

    Inventory Controller

---
+++ SolarCoffee.Web/Controllers/InventoryController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SolarCoffee.Services.Inventory;
+
+namespace SolarCoffee.Web.Controllers
+{
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        private readonly ILogger<InventoryController> _logger;
+        private readonly IInventoryService _inventoryService;
+
+        public InventoryController(ILogger<InventoryController> logger, IInventoryService inventoryService)
+        {
+            _logger = logger;
+            _inventoryService = inventoryService;
+        }
+
+        
+    }
+}
---
+++ SolarCoffee.Web/Controllers/OrderController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SolarCoffee.Services.Customer;
+using SolarCoffee.Services.Order;
+using SolarCoffee.Web.Serialization;
+using SolarCoffee.Web.ViewModels;
+
+namespace SolarCoffee.Web.Controllers
+{
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly ILogger<OrderController> _logger;
+        private readonly IOrderService _orderService;
+        private readonly ICustomerService _customerService;
+
+        public OrderController(ILogger<OrderController> logger, IOrderService orderService, ICustomerService customerService)
+        {
+            _logger = logger;
+            _orderService = orderService;
+            customerService = _customerService;
+        }
+
+        [HttpPost("/api/invoice")]
+        public IActionResult GenerateNewOrder([FromBody] InvoiceModel invoice){
+            _logger.LogInformation("Generating Invoice");
+            var order = OrderMapper.SerializeInvoiceToOrder(invoice);
+            order.Customer = _customerService.GetById(invoice.CustomerId);
+            _orderService.GenerateOpenOrder(order);
+            return Ok();
+        }
+
+        [HttpGet("/api/order")]
+        public IActionResult GetOrders(){
+            var orders = _orderService.GetOrders();
+            var orderModels = OrderMapper.SerializOrderToViewModel(orders);
+            return Ok(orderModels);
+        }
+
+        [HttpPatch("/api/order/complete/{id}")]
+        public IActionResult MarkOrderComplete(int id){
+            _logger.LogInformation("Marking order {id} complete...");
+            _orderService.MarkFulfilled(id);
+            return Ok();
+        }
+
+    }
+}
