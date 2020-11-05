a372bed8ef731f2ff5d693f1edce98c5319cab8e
Daniel Neiva <danielneiva5@outlook.com>
2020-11-05 19:03:43 +0000

    Cypress Testing

--- solarcoffee.frontend/src/views/Inventory.vue
+++ solarcoffee.frontend/src/views/Inventory.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="inventory-container">
-        <h1 class="inventoryTitle">
+        <h1 id="inventoryTitle">
             Inventory Dashboard
         </h1>
         <hr>
